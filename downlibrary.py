a9aaed19d03665321d0279c9b857d3fc9701b273
Ahmed Ramadan <tryandexcept@gmail.com>
2025-08-27 15:30:24 +0300

    updated the version of laibraries

--- downlibrary.py
+++ downlibrary.py
@@ -1,6 +1,7 @@
 import os
-def install_laibrary():
+def install_library():
+    os.system("pip install requests==2.28.2")
     os.system("pip install gTTS==2.3.2")
-    os.system("pip install playdsound==1.2.2")
-    os.system("pip inastall pytssx3")
-    
+    os.system("pip install playsound==1.2.2")
+    os.system("pip install pyttsx3")
+
